20c2b6bb687d1eca9b656ffbf8e989369cf2df81
kabanos2137 <gaweljar0@outlook.com>
2025-04-02 10:53:24 +0200

    Add Django REST framework and update project settings

---
+++ api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+# Create your models here.
+class UserCredentials(models.Model):
+    name =models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
