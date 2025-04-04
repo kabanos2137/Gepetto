3d5e8d9e5a3a138c08086178af5afc3ab54bf0cd
kabanos2137 <gaweljar0@outlook.com>
2025-04-04 23:22:29 +0200

    Add login endpoint to handle user authentication

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import user
+from .views import user, login
 
 urlpatterns = [
     path("user", user, name="user"),
+    path("login", login, name="login"),
 ]
