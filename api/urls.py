d518160750d5c70b0a734ca240f247b74a3c5a8c
kabanos2137 <gaweljar0@outlook.com>
2025-04-05 23:10:28 +0200

    Update Python SDK version and add OpenAI integration with environment variable loading

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,18 @@
+import os
+
 from django.urls import path
+from dotenv import load_dotenv
+
 from .views import user, login
+from openai import AzureOpenAI
+
+load_dotenv("./.confidential.env")
+
+client = AzureOpenAI(
+    azure_endpoint=os.getenv('OPENAI_API_BASE'),
+    api_key=os.getenv('OPENAI_API_KEY'),
+    api_version="2024-05-01-preview",
+)
 
 urlpatterns = [
     path("user", user, name="user"),
